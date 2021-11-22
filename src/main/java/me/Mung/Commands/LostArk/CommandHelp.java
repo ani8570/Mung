c4470500ac5312918bc9d32b48f0a12f10c45ca5
ani8570 <ani8570@naver.com>
2021-11-22 13:05:00 +0900

    edit help

--- src/main/java/me/Mung/Commands/LostArk/CommandHelp.java
+++ src/main/java/me/Mung/Commands/LostArk/CommandHelp.java
@@ -14,9 +14,9 @@ public class CommandHelp implements Command {
     @Override
     public void performCommand(Member m, TextChannel channel, Message message) {
         LOGGER.info(m.getUser().getAsTag());
-        String s = "/조회 (캐릭터명) : 등록한 내 모든(특정) 캐릭터 Lv 조회\n" +
-                "/등록 캐릭터명 : 내 아이디에 캐릭터 등록\n" +
-                "/제거 캐릭터명 : 모든 목록에서 캐릭터 제거";
+        String s = "!조회 (캐릭터명) : 등록한 내 모든(특정) 캐릭터 Lv 조회\n" +
+                "!등록 캐릭터명 : 내 아이디에 캐릭터 등록\n" +
+                "!제거 캐릭터명 : 모든 목록에서 캐릭터 제거";
         message.reply(s).queue();
     }
 }
